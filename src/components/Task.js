cbf511a102be494a7f536d5f8a52070909190bf0
Wen Xiao <wennstyle@gmail.com>
2022-01-30 21:23:59 -0500

    Done.

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function Task() {
+function Task({task, category, onDeleteTask}) {
+  //console.log(task)
+  //prop from TaskList that handles the deleting of tasks. This function takes in one argument, the task itself
+  //To execute the delete, the event listener onClick is used to invoke the function, making sure to include
+  //the argument (the task)
+  function handleClick(){
+    onDeleteTask(task);
+  }
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{task}</div>
+      <button className="delete" onClick={handleClick}>X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+//takes the list of currentTasks stored in the parent component and assigned them each a key, task name
+//and category and then gets sent to Task component as a prop. Also takes in the prop of the
+//function handleDeleteTask. This function gets set to onDeleteTask and passed as a prop to Task component
+function TaskList({currentTasks, onDeleteTask}) {
+  const taskList = currentTasks.map((task)=> (
+    <Task
+    key={task.text}
+    task={task.text}
+    category={task.category}
+    onDeleteTask={onDeleteTask}
+    />
+
+  ))
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+    {taskList}
     </div>
   );
 }
